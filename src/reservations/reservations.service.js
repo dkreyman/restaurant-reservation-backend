f153d202851922c5f81f6eaf06dd3347d765bd47
David Kreyman <dkreyman@gmail.com>
2021-07-10 10:06:07 -0600

    bug fixes & readme update

--- src/reservations/reservations.service.js
+++ src/reservations/reservations.service.js
@@ -12,6 +12,7 @@ function read(date) {
     .select("*")
     .where({ reservation_date: date })
     .whereNot("status", "finished")
+    .whereNot("status", "cancelled")
     .orderBy("reservation_time", "asc");
 }
 
